8aeaa3ea7fe81fb530abe1b55d08a2143ba35c0d
Light Andy <andy.197197@yahoo.com>
2023-12-14 16:26:11 +0200

    Create test_your_module.py
    Default Python test

---
+++ tests/test_your_module.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+class FunctionalTests(unittest.TestCase):
+    def test_your_function1(self):
+        # Add your test logic here
+        pass
+
+    def test_your_function2(self):
+        # Add your test logic here
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
