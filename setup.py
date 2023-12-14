b4e1a706262fc660847051590f883bfaf9b8abd6
Light Andy <andy.197197@yahoo.com>
2023-12-14 19:36:41 +0200

    Update setup.py
    Changed version, added URL, license and changed the Intended Audience to Windows Users.

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="OptiFlowPy",
-    version="1.0.2",
+    version="1.0.3",
+    url="https://github.com/LightAndy/OptiFlowPy",
+    license="MIT",
     author="Light Andy",
     author_email="andy197197@example.com",
     description="An advanced batch image compressor.",
@@ -11,7 +13,7 @@ setup(
     install_requires=["imageio", "numpy", "Pillow", "PyQt5", "tk", "pytest", "wheel"],
     classifiers=[
         "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
+        "Intended Audience :: Windows Users",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
