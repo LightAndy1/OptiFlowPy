2142f0f086527018b0312fe7ad9a3075d9856de9
Light Andy <andy.197197@yahoo.com>
2023-12-14 16:36:24 +0200

    Update setup.py
    Changed version of the setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="OptiFlowPy",
-    version="1.0",
+    version="1.0.1",
     author="Light Andy",
     author_email="andy197197@example.com",
     description="An advanced batch image compressor.",
